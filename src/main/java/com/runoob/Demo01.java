fbc1f066a99b00c6cad7c7ed157c442fb3318728
Eric <neq666666@163.com>
2019-11-20 18:29:46 +0800

    second commit

---
+++ src/main/java/com/runoob/Demo01.java
@@ -0,0 +1,12 @@
+package com.runoob;
+
+/**
+ * @author Eric Ning
+ * @version 1.0
+ * @date 2019/11/20 18:29
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
