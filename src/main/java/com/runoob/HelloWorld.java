fa5e16d75a09e5d7f600a5931266f801ecac6c21
Eric <neq666666@163.com>
2019-11-20 18:18:59 +0800

    first commit

---
+++ src/main/java/com/runoob/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.runoob;
+
+/**
+ * @author Eric Ning
+ * @version 1.0
+ * @date 2019/11/20 18:14
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
